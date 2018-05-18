aed7cd2cf2601b6a810c4b0ce06468d39530ce5e
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-05-18 14:32:39 +0800

    修改背景圖刷新bug

--- js/headerImage.js
+++ js/headerImage.js
@@ -10,7 +10,8 @@ var maxNum = 8;
 var minNum = 1;
 var random = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-const Imageurl = `${img.baseURI}img/header/bg${random}.png`;
+// const Imageurl = `${img.baseURI}img/header/bg${random}.png`;
+const Imageurl = `https://andy6804tw.github.io/ecare-cjcu-sheepherd-blog/img/header/bg${random}.png`;
 
 // 載入隨機更換背景照片
 img.style.backgroundImage = `url(${Imageurl})`;
